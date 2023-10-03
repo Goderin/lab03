16e3110092baf0d0d5954d219d1c96fb9dcbb8fb
Vladimir <117534215+Goderin@users.noreply.github.com>
2023-10-03 17:07:32 +0500

    new-feature

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ arr = []
 for i in range(n):
     arr.append(int(input("Введите число: ")))
 
-for i in range(n-1):
-    for j in range(n-1-i):
-        if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
+direction = input("Выберите направление сортировки (возрастание/убывание): ")
+if direction == "возрастание":
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+elif direction == "убывание":
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if arr[j] < arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 print("Отсортированный массив:")
 for i in arr:

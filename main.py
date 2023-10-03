a3da7f4ade6d0d7d93c197734a8b4e8ae8144992
Vladimir <117534215+Goderin@users.noreply.github.com>
2023-10-03 17:03:55 +0500

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int(input("Введите количество чисел: "))
+arr = []
+for i in range(n):
+    arr.append(int(input("Введите число: ")))
+
+for i in range(n-1):
+    for j in range(n-1-i):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+print("Отсортированный массив:")
+for i in arr:
+    print(i, end=" ")
